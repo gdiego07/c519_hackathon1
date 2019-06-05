0f70f2396d09033e53214af709e733dcd6bf4b63
raul jauregui <felx90@outlook.com>
2019-06-05 16:43:10 -0700

    added carrot to players hand and option to plant

--- 5_the_happy_farm/main.js
+++ 5_the_happy_farm/main.js
@@ -2,5 +2,30 @@ $(document).ready(initializeApp);
 
 
 function initializeApp() {
-    
+    new DealOneCardToPlayerEachTurn();
+    new Plant();
+}
+
+
+
+class DealOneCardToPlayerEachTurn {
+
+    constructor(){
+        $(".playerCropCard").text("carrot click to plant");
+    }
+
+    changeCard (){
+
+    }
+
+}
+class Plant {
+
+    constructor(click){
+        $(".playerCropCard").on("click",this.plant);
+    }
+    plant(){
+        $(".spring").append(" carrot has been planted");
+        $(".playerCropCard").text("no cards");
+    }
 }
