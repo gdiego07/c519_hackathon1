fc128aa0daa089386769f363cc5561852dabca4b
Brandon Collette <Brandoncollette24@gmail.com>
2019-06-05 17:05:51 -0700

    add money

--- 5_the_happy_farm/main.js
+++ 5_the_happy_farm/main.js
@@ -1,6 +1,22 @@
 $(document).ready(initializeApp);
 
-
+var coinAction;
 function initializeApp() {
-    
+ coinAction = new takeCoinAction();
+}
+
+class takeCoinAction {
+    constructor() {
+         this.moneyAmount = 2;
+         $('.moneySack').text(this.moneyAmount);
+        $('.takeCoin').on('click', this.addCoin.bind(this));
+    }
+
+
+    addCoin() {
+        console.log('addcoin');
+           this.moneyAmount += 1 ;
+        $('.moneySack').text(this.moneyAmount);
+        console.log(this.moneyAmount)
+    }
 }
