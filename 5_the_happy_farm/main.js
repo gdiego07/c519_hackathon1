f894b4b1171c9d86e3c9622b198cb4008f15a0b9
Diego Garcia <d.garcia07@outlook.com>
2019-06-05 18:04:24 -0700

    User Animal

--- 5_the_happy_farm/main.js
+++ 5_the_happy_farm/main.js
@@ -2,5 +2,25 @@ $(document).ready(initializeApp);
 
 
 function initializeApp() {
-    
+    new UserAnimalCards();
+}
+class UserAnimalCards {
+    constructor() {
+        this.sadFace = true;
+        $(".playerAnimalCard").on("click", this.getSadFaceAnimal);
+        $(".playerAnimalCard").on("click", this.getAnimalTail);
+        
+    }
+    getSadFaceAnimal() {
+        $('.frontAnimalCard').text('cowSadFace');
+      console.log('Sad')
+       
+    }
+    getAnimalTail() {
+       $('.tailAnimalCard').text('cowTail'); 
+      console.log('Im a tail')
+    }
+    addHappyFace() {
+        $(".frontAnimalCard").text("cowHappyFace");
+    }
 }
