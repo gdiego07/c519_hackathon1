1173de688a35f697ee0b517696e57e76797f0b22
Diego Garcia <d.garcia07@outlook.com>
2019-06-06 12:05:14 -0700

    Trying to Git

--- 5_the_happy_farm/main.js
+++ 5_the_happy_farm/main.js
@@ -6,39 +6,44 @@ function initializeApp() {
     new DealOneCardToPlayerEachTurn();
     new Plant();
     new MoveCardUpFarmSeasons();
-    coinAction = new takeCoinAction()
+    coinAction = new takeCoinAction();
     new UserAnimalCards();
-
-
+    new CompareFeedToHarvest();
+    new BuyCropCard();
 }
+
 class UserAnimalCards {
     constructor() {
         this.sadFace = true;
         $(".playerAnimalCard").on("click", this.getSadFaceAnimal);
         $(".playerAnimalCard").on("click", this.getAnimalTail);
-        
+
     }
+
     getSadFaceAnimal() {
         $('.frontAnimalCard').text('cowSadFace -3');
-      console.log('Sad')
-       
+        $('.frontAnimalCard').addClass('sadcowimage');
+        console.log('Sad')
+
     }
+
     getAnimalTail() {
-       $('.tailAnimalCard').text('cowTail'); 
-      console.log('Im a tail')
+        $('.tailAnimalCard').text('cowTail');
+        console.log('Im a tail')
     }
+
     addHappyFace() {
         $(".frontAnimalCard").text("cowHappyFace");
     }
 }
-
 class DealOneCardToPlayerEachTurn {
 
-    constructor(){
+    constructor() {
         $(".playerCropCard").text("carrot click to plant");
+        $('.playerCropCard').addClass('carrotimage')
     }
 
-    changeCard (){
+    changeCard() {
 
     }
 
@@ -52,6 +57,8 @@ class Plant {
     plant() {
         $(".spring").append(" carrot has been planted");
         $(".playerCropCard").text("no cards");
+        $('.playerCropCard').removeClass('carrotimage');
+        $('.spring').addClass('carrotimage');
     }
 }
 
@@ -65,20 +72,28 @@ class MoveCardUpFarmSeasons {
 
     moveUpToSummer(){
         $(".spring").text("spring");
+        $('.spring').removeClass('carrotimage');
         $(".summer").append(" carrot can be harvest");
+        $('.summer').addClass('carrotimage');
+
     }
     moveUpToAutumn(){
         $(".summer").text("summer");
+        $('.summer').removeClass('carrotimage');
         $(".autumn").append(" carrot harvest in autumn");
+        $('.autumn').addClass('carrotimage');
     }
     storeInFarm(){
         $(".autumn").text("autumn");
-        $(".farmBoard").append(" carrot harvest in autumn");
+        $('.autumn').removeClass('carrotimage');
+        $('.farmBoard').addClass('carrotimage');
+        $(".farmBoard").append(" carrot ready to buy feed card");
+        $(".farmBoard").addClass("carrot")
     }
        
 }
 class takeCoinAction {
-    constructor() {
+    constructor () {
          this.moneyAmount = 2;
          $('.moneySack').text(this.moneyAmount);
         $('.takeCoin').on('click', this.addCoin.bind(this));
@@ -93,3 +108,49 @@ class takeCoinAction {
 
     }
 }
+class CompareFeedToHarvest {
+
+    constructor() {
+        $(".feedingCard1").on("click", this.compareCards);
+        $('.frontAnimalCard').removeClass('sadcowimage');
+
+    }
+
+    compareCards() {
+        if ($(".farmBoard").hasClass("carrot")&& $(".feedingCard1").hasClass("carrot")) {
+            $(".frontAnimalCard").text("happy cow 9000");
+            $('.farmBoard').removeClass('carrotimage');
+            $('.frontAnimalCard').removeClass('sadcowimage');
+            $('.frontAnimalCard').addClass('happycowimage');
+            alert('Your cow was fed, you win!');
+
+        }
+    }
+}
+class BuyCropCard {
+
+    constructor(){
+        $(".CropCard1").on("click",this.buyCropCard)
+    }
+    buyCropCard(){
+        if ($(".moneySack").text()>=1) {
+            $(".playerCropCard").text("carrot");
+          var x=  $(".moneySack").text();
+          var y = parseInt(x);
+          var z = --y;
+          $(".moneySack").text(z);
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
